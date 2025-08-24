48328a6aff806efb35a0f3ef928760a965600163
Froggyuwuw <uwujuanda6@gmail.com>
2025-08-23 20:35:03 -0500

    ola

--- PROYECTO/src/main/java/APP/domain/model/User.java
+++ PROYECTO/src/main/java/APP/domain/model/User.java
@@ -2,29 +2,87 @@ package APP.domain.model;
 import APP.domain.model.emuns.Role;
 
 public class User {
-    private Long id;
-    private String name;
-    private String email;
-    private Role role;
+    private Long id;          // id interno
+    private String document;  // cedula o documento
+    private String name;      // nombre real
+    private String username;  // nombre de usuario
+    private String email;     // correo electrónico
+    private String password;  
+    private String role;      
 
-   
-    public User(Long id, String name, String email, Role role) {
+    public User() {
+     
+    }
+
+    public User(Long id, String document, String name, String username, String email, String password, String role) {
         this.id = id;
+        this.document = document;
         this.name = name;
+        this.username = username;
         this.email = email;
+        this.password = password;
         this.role = role;
     }
 
-    
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    // Getters & Setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public String getDocument() {
+        return document;
+    }
+    public void setDocument(String document) {
+        this.document = document;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public Role getRole() { return role; }
-    public void setRole(Role role) { this.role = role; }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", document='" + document + '\'' +
+                ", name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
