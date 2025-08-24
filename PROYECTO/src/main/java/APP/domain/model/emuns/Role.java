34c97d2f8a6a4a52bef61bd47cd1aa5259346ffb
Syberiano <syberiano.2704@gmail.com>
2025-08-23 20:31:11 -0500

    ADD

--- PROYECTO/src/main/java/APP/domain/model/emuns/Role.java
+++ PROYECTO/src/main/java/APP/domain/model/emuns/Role.java
@@ -1,17 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package APP.domain.model.emuns;
 
 
-public class Role {
+public enum Role {
     ADMINISTRATOR,
     DOCTOR,
     NURSE,
     PATIENT,
     RECEPTIONIST,
     PHARMACIST,
-    LAB_TECHNICIAN
+    SUPPORT,
     
 }
