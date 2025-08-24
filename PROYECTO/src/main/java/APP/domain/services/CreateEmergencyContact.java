6536607c5bf8616a4bf06a1bef91eaeffb1e1a9b
Froggyuwuw <uwujuanda6@gmail.com>
2025-08-23 21:46:49 -0500

    service emergencycontact

--- PROYECTO/src/main/java/APP/domain/services/CreateEmergencyContact.java
+++ PROYECTO/src/main/java/APP/domain/services/CreateEmergencyContact.java
@@ -1,8 +1,30 @@
-
 package APP.domain.services;
 
-public class CreateEmergencyContact  extends EmergencyContact{
-    
-    public sta
-    
+import APP.domain.model.EmergencyContact;
+import APP.domain.ports.EmergencyContactPort;
+
+public class CreateEmergencyContact {
+
+    private EmergencyContactPort emergencyContactPort;
+
+    // Constructor vacío
+    public CreateEmergencyContact() {}
+
+    // Constructor con dependencia
+    public CreateEmergencyContact(EmergencyContactPort emergencyContactPort) {
+        this.emergencyContactPort = emergencyContactPort;
+    }
+
+    // Setter para poder inyectar el port después
+    public void setEmergencyContactPort(EmergencyContactPort emergencyContactPort) {
+        this.emergencyContactPort = emergencyContactPort;
+    }
+
+    public void create(EmergencyContact emergencyContact) throws Exception {
+        if (emergencyContactPort.findByPhone(phone)!= null) {
+            throw new Exception("Ya existe un contacto registrado con ese número de teléfono");
+        }
+
+        emergencyContactPort.save(emergencyContact);
+    }
 }
