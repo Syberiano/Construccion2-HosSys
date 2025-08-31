34db9978ed672d062d2ea86a55620a7539549326
Marco_Ch <marcochavezalgarin12@gmail.com>
2025-08-31 15:32:07 -0500

    actualizando entidades

--- PROYECTO/src/main/java/APP/domain/model/DiagnosticTest.java
+++ PROYECTO/src/main/java/APP/domain/model/DiagnosticTest.java
@@ -8,46 +8,59 @@ public class DiagnosticTest {
     private String name;
     private String description;
     private double cost;
+    private Specialty specialty;
 
-    public DiagnosticTest(String id, String name, String description, double cost) {
+    public DiagnosticTest(String id, String name, String description, double cost, Specialty specialty) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.cost = cost;
+        this.specialty = specialty;
     }
-    
-    
 
     public String getId() {
         return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public double getCost() {
-        return cost;
     }
 
     public void setId(String id) {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public void setDescription(String description) {
         this.description = description;
     }
 
+    public double getCost() {
+        return cost;
+    }
+
     public void setCost(double cost) {
         this.cost = cost;
     }
+
+    public Specialty getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(Specialty specialty) {
+        this.specialty = specialty;
+    }
+
+    
+
+
+   
     
 }
--- PROYECTO/src/main/java/APP/domain/model/Procedure.java
+++ PROYECTO/src/main/java/APP/domain/model/Procedure.java
@@ -8,56 +8,60 @@ public class Procedure {
     private String name;
     private String description;
     private double cost;
-    private String specialtyId;
+    private Specialty specialty;
 
-    public Procedure(String id, String name, String description, double cost, String specialtyId) {
+
+    public Procedure(String id, String name, String description, double cost, Specialty specialty) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.cost = cost;
-        this.specialtyId = specialtyId;
+        this.specialty = specialty;
     }
-    
-    
 
     public String getId() {
         return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public double getCost() {
-        return cost;
-    }
-
-    public String getSpecialtyId() {
-        return specialtyId;
     }
 
     public void setId(String id) {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public void setDescription(String description) {
         this.description = description;
     }
 
+    public double getCost() {
+        return cost;
+    }
+
     public void setCost(double cost) {
         this.cost = cost;
     }
 
-    public void setSpecialtyId(String specialtyId) {
-        this.specialtyId = specialtyId;
+    public Specialty getSpecialty() {
+        return specialty;
     }
+
+    public void setSpecialty(Specialty specialty) {
+        this.specialty = specialty;
+    }
+    
+    
+    
+
+   
     
 }
